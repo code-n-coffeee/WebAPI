27fe145d1ff3f2da5aa0b72de16c8d72ef0a05ba
Ruby kumari <rubykmri488@gmail.com>
2021-02-16 10:25:02 +0530

    Add project files.

---
+++ WebAPI/Controllers/StudentController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET api/<controller>
+
+        //Declare sql connection and command objects here
+
+        private SqlConnection conn;
+        private SqlDataAdapter adapter;
+        public IEnumerable<Student> Get()
+        {
+            conn = new SqlConnection("data source=DESKTOP-3CTJ57L\\SQLEXPRESS; Initial catalog=SampleDB; user id=sa; password=orangejuice;");
+            DataTable dt = new DataTable();
+            var query = "select * from Student";
+            adapter = new SqlDataAdapter
+
+            { SelectCommand = new SqlCommand(query, conn)
+              };
+
+            adapter.Fill(dt);
+            List<Student> st = new List<Models.Student>(dt.Rows.Count);
+            if(dt.Rows.Count>0)
+            {
+                foreach(DataRow studentrecord in dt.Rows)
+                {
+                    st.Add(new ReadStudent(studentrecord));
+                }
+            }
+
+            return st;
+        }
+
+        // GET api/<controller>/5
+      public IEnumerable<Student> Get(int id)
+        {
+            conn = new SqlConnection("data source=DESKTOP-3CTJ57L\\SQLEXPRESS; Initial catalog=SampleDB; user id=sa; password=orangejuice;");
+            DataTable dt = new DataTable();
+            var query = $"select * from Student where id={id}";
+            adapter = new SqlDataAdapter
+
+            {
+                SelectCommand = new SqlCommand(query, conn)
+            };
+
+            adapter.Fill(dt);
+            List<Student> st = new List<Models.Student>(dt.Rows.Count);
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow studentrecord in dt.Rows)
+                {
+                    st.Add(new ReadStudent(studentrecord));
+                }
+            }
+
+            return st;
+        }
+
+
+        // POST api/<controller>
+        public string Post([FromBody] createStudent value)
+        {
+            conn = new SqlConnection("data source=DESKTOP-3CTJ57L\\SQLEXPRESS; Initial catalog=SampleDB; user id=sa; password=orangejuice;");
+            var query = "insert into Student(Id,FullName,Course,ContactNumber) values(@Id,@FullName,@Course,@ContactNumber)";
+            SqlCommand insertCommand = new SqlCommand(query, conn);
+
+            insertCommand.Parameters.AddWithValue("@Id", value.Id);
+            insertCommand.Parameters.AddWithValue("@FullName", value.FullName);
+            insertCommand.Parameters.AddWithValue("@Course", value.Course);
+            insertCommand.Parameters.AddWithValue("@ContactNumber", value.ContactNumber);
+            conn.Open();
+            int result = insertCommand.ExecuteNonQuery();
+            if(result>0)
+            {
+                return "Inserted ";
+            }
+
+            else
+            {
+                return "Not Inserted";
+            }
+
+         }
+
+        // PUT api/<controller>/5
+        public string Put(int id, [FromBody] createStudent value)
+        {
+
+            conn = new SqlConnection("data source=DESKTOP-3CTJ57L\\SQLEXPRESS; Initial catalog=SampleDB; user id=sa; password=orangejuice;");
+            var query = "Update Student set Id=@Id,FullName=@FullName,Course=@Course,ContactNumber=@ContactNumber where Id=" + id;
+            SqlCommand insertCommand = new SqlCommand(query, conn);
+
+            insertCommand.Parameters.AddWithValue("@Id", value.Id);
+            insertCommand.Parameters.AddWithValue("@FullName", value.FullName);
+            insertCommand.Parameters.AddWithValue("@Course", value.Course);
+            insertCommand.Parameters.AddWithValue("@ContactNumber", value.ContactNumber);
+            conn.Open();
+            int result = insertCommand.ExecuteNonQuery();
+            if (result > 0)
+            {
+                return "True";
+            }
+
+            else
+            {
+                return "False";
+            }
+        }
+
+        // DELETE api/<controller>/5
+        public string Delete(int id)
+        {
+            conn = new SqlConnection("data source=DESKTOP-3CTJ57L\\SQLEXPRESS; Initial catalog=SampleDB; user id=sa; password=orangejuice;");
+            var query = "Delete from Student where Id="+ id;
+            SqlCommand insertCommand = new SqlCommand(query, conn);
+
+            conn.Open();
+            int result = insertCommand.ExecuteNonQuery();
+            if (result > 0)
+            {
+                return "True";
+            }
+
+            else
+            {
+                return "False";
+            }
+        }
+    }
+}
---
+++ WebAPI/Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace WebAPI.Models
+{
+    public class Student
+    { //Define all the properties of table with get and set
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Course { get; set; }
+        public int ContactNumber { get; set; }
+    }
+
+    //Creating an object of Student class
+    public class createStudent : Student
+    {
+
+    }
+
+    public class ReadStudent: Student 
+    {
+       public ReadStudent(DataRow row)
+        {
+            Id = Convert.ToInt32(row["Id"]);
+            FullName = row["FullName"].ToString();
+            Course = row["Course"].ToString();
+            ContactNumber= Convert.ToInt32(row["ContactNumber"]);
+
+        }
+
+
+    }
+}
